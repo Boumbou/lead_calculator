30388ff8f82b32f5df51b779e2fa39f3b84c8d76
Badre E.A <badre@b.be>
2022-01-03 00:33:15 +0100

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import businessCaseReducer from '../businessCases/businessCaseSlice';
-
-export default configureStore({
-  reducer: {
-    businessCase: businessCaseReducer
-  }
-})
